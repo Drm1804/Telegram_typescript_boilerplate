1e8ca952aa770ffcd50a488bfabe53ec1a8af9ca
Alex Four <>
2025-02-26 10:32:38 +0100

    migrate to dotenv

---
+++ config.ts
@@ -0,0 +1,20 @@
+import { Config } from './config.type.js';
+import 'dotenv/config';
+
+export const conf: Config = {
+  authFirebase: {
+    email: process.env.AUTH_FIREBASE_EMAIL,
+    password: process.env.AUTH_FIREBASE_PASSWORD,
+  },
+  firebase: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+  },
+  botToken: process.env.BOT_TOKEN,
+  storagePath: `${process.cwd()}/files/`,
+};
